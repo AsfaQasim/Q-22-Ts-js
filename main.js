309589a0a17be38eeab42fecb0140efd2832ad44
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 08:25:03 -0800

    Q 22 of Ts/js & node js of 45 assignment.
    Q 22 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,4 @@
+var array = ["Babar Azam", "Taha", 3, 6, 7, "Sana"];
+console.log(array[5]);
+console.log(array[0]);
+console.log(array[1]);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let array:(string | number) [] = [`Babar Azam`,`Taha`,3,6,7,`Sana`]
+console.log(array[5]);
+console.log(array[0]);
+console.log(array[1]);
